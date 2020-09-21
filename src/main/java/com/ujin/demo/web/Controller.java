f9bb762f3c1aeaa81e5073394ae81495f0e6bec7
YujinAhn <ujahnn@gmail.com>
2020-09-21 21:47:51 +0900

    jpa, h2 설정

---
+++ src/main/java/com/ujin/demo/web/Controller.java
@@ -0,0 +1,20 @@
+package com.ujin.demo.web;
+
+import com.ujin.demo.web.dto.ResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    @GetMapping("/hello/dto")
+    public ResponseDto helloDTO(@RequestParam("name") String name,
+                                @RequestParam("amount") int amount){
+        return new ResponseDto(name, amount);
+    }
+}
---
+++ src/test/java/com/ujin/demo/web/dto/ResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.ujin.demo.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        ResponseDto dto = new ResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
